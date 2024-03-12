6e6ac20f373718cfa091f5651512162c2ead2e00
evanb <labber@Evangelos>
2024-03-12 13:39:04 -0400

    adding favorites to the code

--- routes/users-api.js
+++ routes/users-api.js
@@ -22,3 +22,30 @@ router.get('/', (req, res) => {
 });
 
 module.exports = router;
+
+router.post('/:userId/favorites', (req, res) => {
+  const { userId } = req.params;
+  const { itemId } = req.body;
+  const query = `INSERT INTO favorites (userID, itemID) VALUES (${userId}, ${itemId})`;
+  db.query(query)
+    .then(() => {
+      res.status(201).send('Favorite added');
+    })
+    .catch(err => {
+      res.status(500).json({ error: err.message });
+    });
+});
+
+
+router.get('/:userId/favorites', (req, res) => {
+  const { userId } = req.params;
+  const query = `SELECT * FROM product WHERE itemID IN (SELECT itemID FROM favorites WHERE userID = ${userId})`;
+  db.query(query)
+    .then(data => {
+      const favorites = data.rows;
+      res.json({ favorites });
+    })
+    .catch(err => {
+      res.status(500).json({ error: err.message });
+    });
+});
