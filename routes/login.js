1692119c0d9885d1286e9214bb902c86c0923c03
Erin Von Gertzen <ianvon@shaw.ca>
2024-03-12 12:26:15 -0600

    Login.js, Register.js now works and gives cookie using cookie session.

--- routes/login.js
+++ routes/login.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db/connection');
-//const cookieSession = require("cookie-session");
+const cookieSession = require("cookie-session");
 
 const getUser = function (user_email, user_password) {
   const sqlQuery = `SELECT * FROM users WHERE email = $1;`;
   const sqlValues = [user_email];
   return db.query(sqlQuery, sqlValues)
     .then(data => {
+      console.log("getuseroutput:");
+      console.log(data);
       const user = data.rows[0];
       return user;
     })
@@ -32,16 +34,20 @@ router.get('/', (req, res, next) => {
 })
 
 router.post('/', (req, res, next) => {
+  console.log("login post");
   const email = req.body.email;
   const password = req.body.password;
   getUser(email, password)
     .then(user => {
       if(user && user.password === password){
-        req.session.user_id = user.id;
-        res.redirect('index')
+        console.log("userid");
+        console.log(user.userid);
+        req.session.user_id = user.userid;
+        res.redirect('/')
         return;
       }
-      res.redirect('/login')
+
+      res.status(403).send("Incorrect Password");
     })
 
 
---
+++ routes/logout.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.post("/logout", (req, res) => {
+  req.session = null;
+  res.redirect("/login");
+});
+
+
+module.exports = router;
